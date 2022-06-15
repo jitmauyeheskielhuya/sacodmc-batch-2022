e4572a67e3b0f4b1807805beea7dcd292efcdb84
jitmauyeheskielhuya <jitmauyeheskiel@mail.com>
2022-06-15 21:30:33 +0900

    menambahkan folder day02 dan file di dalanya

---
+++ day02/project01/create-proses.php
@@ -0,0 +1,17 @@
+<?php
+
+$first_name = $_GET['first_name'];
+$last_name = $_GET['last_name'];
+
+?>
+
+
+
+<h3>Create Proses</h3>
+
+<p>
+  first name : <?= $first_name; ?>
+
+<p>
+  last name : <?= $last_name; ?>
+</p>
